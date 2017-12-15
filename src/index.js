6e41cb35b74d519ed5dfc929ac080dc0ce359e5f
Binh Khuu <binhkhuu90@gmail.com>
2017-12-15 13:26:15 +0800

    added camper data module

--- src/index.js
+++ src/index.js
@@ -1,32 +1,54 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-var https = require('https');
+
 
 class Camper extends React.Component {
 	render() {
 		return (
 			<tr>
-				<th scope='col'>{this.props.index}</th>
-				<td>Camper</td>
-				<td>Camper</td>
-				<td>Camper</td>
+				<td scope='col'>{this.props.index}</td>
+				<td>{this.props.id.username}</td>
+				<td>{this.props.id.score}</td>
+				<td>{this.props.id.overall}</td>
 			</tr>
 		);
 	}
 }
 
 class Ladder extends React.Component {
-
+	constructor() {
+		super();
+		this.state = {
+			campers: [{username:"loading",score:0,overall:0}]
+		}
+	}
+	componentWillMount() {
+		var obj = {username:"",score: 0, overall: 1};
+		fetch('https://fcctop100.herokuapp.com/api/fccusers/top/recent').then(response =>{
+			return response.json();
+		}).then(object =>{	
+			let data = object.map((d) => {
+				var rObj = {username:'',score: 0};
+				rObj['username'] = d.username
+				rObj['score'] = d.recent;
+				rObj['overall'] = d.alltime;
+				return rObj;
+			})
+			this.setState({campers:data})
+		})
+		
+	}
   render() {
+  	console.log(this.state.campers[0].username)
   	var ladder = [];
   	for(var i = 0; i< 100; i++){
   		ladder[i] = i;
   	}
     return (
-    	<div class='background'>
-    		<table class='table table-striped' align='right'>
-    			<thead class='thead-dark'>
+    	<div className='background'>
+    		<table className='table table-striped' align='right'>
+    			<thead className='thead-dark'>
     				<tr>
 							<th scope='col' width='15%'>Rank</th>
 							<th scope='col' width='20%'>Camper</th>
@@ -36,7 +58,9 @@ class Ladder extends React.Component {
     			</thead>
     			<tbody>
 		    		{ladder.map((x,i) => {
-		    			return <Camper index={i+1}/>
+		    			//check if http request in componetwillmount has set the state
+		    			if(this.state.campers[i] == undefined) i = 0;
+		    			return <Camper index={i+1} id={this.state.campers[i]}/>
 		    		})}    				
     			</tbody>
     		</table>
