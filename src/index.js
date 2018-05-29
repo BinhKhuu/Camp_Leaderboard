c97fc02b2b9bd0d6ecc7d3ec6501d105e1608f47
BinhKhuu <binhkhuu90@gmail.com>
2018-05-29 16:07:16 +0800

    fixed indentation issue with sublime text

--- src/index.js
+++ src/index.js
@@ -2,7 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-
+/* Camper information element 
+ * @props.id, object containing infomration about a camper
+ *	object attribes listed here: https://fcctop100.herokuapp.com/api/fccusers/top/recent
+ * @props.index, index location on ladder for current camper
+ */
 class Camper extends React.Component {
 	render() {
 		return (
@@ -19,19 +23,21 @@ class Camper extends React.Component {
 class Ladder extends React.Component {
 	constructor() {
 		super();
-		//set default state
 		this.state = {
-			campers: 	[{	
-									username:"loading...",
-									recent:0,
-									alltime:0,
-									img:'loading...',
-								}]
+			campers: [{	
+				username:"loading...",
+				recent:0,
+				alltime:0,
+				img:'loading...',
+			}]
 		}
 	}
 	componentWillMount() {
 		this.getPoints('https://fcctop100.herokuapp.com/api/fccusers/top/recent');			
 	}
+	/* getCamper object
+	 * @url, url of the api call made to get camper object
+	 */
 	getPoints(url) {
 		fetch(url).then(response =>{
 			return response.json();
@@ -39,35 +45,49 @@ class Ladder extends React.Component {
 			this.setState({campers:object})
 		})
 	}
-  render() {
+	render() {
   	//set ladder to be size 100 for the loop in the table body
-  	var ladder = [];
-  	for(var i = 0; i< 100; i++){
-  		ladder[i] = i;
-  	}
-    return (
-    	<div className='background'>
-    		<table className='table table-striped' align='right'>
-    			<thead className='thead-dark'>
-    				<tr>
-							<th scope='col' width='10%'>Rank</th>
-							<th scope='col' width='25%'>Camper</th>
-							<th scope='col' width='30%'><a class='header-links' href='#' onClick={() => this.getPoints('https://fcctop100.herokuapp.com/api/fccusers/top/recent')}>Points in past 30 Days </a></th>
-							<th scope='col' ><a class='header-links' href='#' onClick={() => this.getPoints('https://fcctop100.herokuapp.com/api/fccusers/top/alltime')}> Total Points</a></th>
-						</tr>
-    			</thead>
-    			<tbody>
-		    		{ladder.map((x,i) => {
-		    			//check if http request in componetWillMount and getPoints has set the state
-		    			if(this.state.campers[i] == undefined) i = 0;
-		    			return <Camper index={i+1} id={this.state.campers[i]}/>
-		    		})}    				
-    			</tbody>
-    		</table>
-
-    	</div>
-    );
-  }
+  		var ladder = [];
+  		for(var i = 0; i< 100; i++){
+			ladder[i] = i;
+		}
+    	return (
+		<div className='background'>
+			<table className='table table-striped' align='right'>
+				<thead className='thead-dark'>
+					<tr>
+						<th scope='col' width='10%'>Rank</th>
+						<th scope='col' width='25%'>Camper</th>
+						<th scope='col' width='30%'>
+							<a 
+								class='header-links' 
+								href='#' onClick={() => 
+								this.getPoints('https://fcctop100.herokuapp.com/api/fccusers/top/recent')}
+							>
+								Points in past 30 Days 
+							</a>
+					</th>
+						<th scope='col' >
+							<a class='header-links' 	
+								href='#' 
+								onClick={() => this.getPoints('https://fcctop100.herokuapp.com/api/fccusers/top/alltime')}
+								> 
+								Total Points
+							</a>
+						</th>
+					</tr>
+				</thead>
+				<tbody>
+					{ladder.map((x,i) => {
+						//check if http request in componetWillMount and getPoints has set the state
+						if(this.state.campers[i] == undefined) i = 0;
+						return <Camper index={i+1} id={this.state.campers[i]}/>
+					})}    				
+				</tbody>
+			</table>
+		</div>
+    	);
+	}
 }
 
 ReactDOM.render(
